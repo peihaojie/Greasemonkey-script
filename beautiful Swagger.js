4002634a8facb7c9044b3a37f45b1e9807305f6b
haojie <784184859@qq.com>
2021-10-09 09:36:39 +0800

    init

---
+++ beautiful Swagger.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          beautiful Swagger
+// @namespace     http://www.example.com/gmscripts
+// @description   让Swagger变得更加好用
+// @include       https://github.com/peihaojie/Greasemonkey-script/*
+// @version       1.0
+// @icon          https://github.com/peihaojie/Greasemonkey-script/icon.png
+// ==/UserScript==
+
+console.log(GM.info)
